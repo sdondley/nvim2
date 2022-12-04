61f574edf1a5630af035862084a8cb4754d8d0fe
Steve Dondley <s@dondley.com>
2022-12-04 11:18:20 -0500

    add config for vimwiki

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,30 @@
-require('init_basic');
+vim.cmd[[
+let mapleader=';'
+filetype plugin on
+filetype indent on
+syntax on
+set nocompatible
+set mouse=a
+set clipboard=unnamedplus
+set background=dark
+set tabstop=4
+set hlsearch
+set incsearch
+set expandtab
+set number
+"set cpt=.,w,b,u,t
+set softtabstop=4
+set shiftwidth=4
+set autoindent
+set number
+set backupdir=~/.cache/nvim
+set ignorecase
+set smartcase
+set timeoutlen=400
+"set fo-=c
+"set fo-=o
+"set fo-=r
+"set fo+=t
+]]
 require('plugins')
-require('init_tmux')
+vim.cmd[[ source ~/.config/nvim/config/tmux.vim ]]
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,8 @@
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use { 
-      { 'wbthomason/packer.nvim', opt = false },
-      { 'christoomey/vim-tmux-navigator',	    opt = true, cond = true },
-      { 'jebaum/vim-tmuxify', 		    opt = true, cond = true },
+      { 'wbthomason/packer.nvim' },
+      { 'christoomey/vim-tmux-navigator' },
+      { 'jebaum/vim-tmuxify' },
+      { 'vimwiki/vimwiki', branch = 'dev' },
   }
   end)
